ca6617af177f568eec5b58bccca3dbc0c7373082
25padarthi <70246578+25padarthi@users.noreply.github.com>
2020-08-26 09:52:44 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<body>
+<?php echo "Hello World! This is the Version 1 App "; ?>
+<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
+<?php
+}
+?>
+</body>
+</html>
