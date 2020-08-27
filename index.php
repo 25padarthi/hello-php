387a6a95df17796983871fc620dc6f347a1226d1
25padarthi <70246578+25padarthi@users.noreply.github.com>
2020-08-27 21:37:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <body>
 <?php echo "Hello World! This is the Version 1 App "; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
+<?php echo "This Program is written by vinod "; ?>
 <?php
 }
 ?>
