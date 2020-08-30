15c91c7f6c42c93fc4cd0ab7f3053af54c32b016
root <root@workstation.lab.example.com>
2020-08-30 05:08:56 -0400

    updated app

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <body>
 <?php echo "Hello World! This is the Version 1 App "; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
-<?php echo "This Program is written by vinod "; ?>
-<?php echo "A Change is Coming!"; ?>
+<?php echo "This Program is written by vinod "; ?><br /><br />
+<?php echo "A Change is Coming in Air!"; ?>
 <?php
 }
 ?>
