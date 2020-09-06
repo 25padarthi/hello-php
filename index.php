b4d75a120957cab88146394f88f50d6cacecc6e7
root <root@docker.lab.example.com>
2020-09-06 07:56:26 -0400

    add configuration

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <?php echo "Hello World! This is the Version 1 App "; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 <?php echo "This Program is written by vinod "; ?><br /><br />
-<?php echo "I have changed this"; ?>
+<?php echo "Change is coming in the Air"; ?>
+
 <?php
 }
 ?>
