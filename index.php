32439710effcb1942152361042ce1afa479ecece
root <root@master.lab.example.com>
2020-09-19 02:49:20 -0400

    Updated Page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php echo "Hello World! This is the Version 1 App "; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 <?php echo "This Program is written by vinod "; ?><br /><br />
-<?php echo "S2I"; ?>
+<?php echo "This is My S2I Image"; ?>
 
 <?php
 }
